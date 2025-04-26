c4e91ca36803007942d67b50f6631c93214e3c8d
Julie Radant <jura0002@stud.kea.dk>
2025-04-26 15:40:49 +0200

    uploader

---
+++ nuxt-app/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <h1 class="bg-green-200 text-4xl p-4 rounded-lg shadow-md">forside</h1>
+  </div>
+</template>
---
+++ nuxt-app/pages/tester.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1 class="text-blue-200">tester</h1>
+    <p>tester para</p>
+  </div>
+</template>
